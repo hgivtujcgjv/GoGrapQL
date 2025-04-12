4732db4088371c7027cca9b24bff91effd37574f
Ekke <112762784+hgivtujcgjv@users.noreply.github.com>
2025-04-12 23:58:11 +0300

    Update resolver.go

--- resolver.go
+++ resolver.go
@@ -8,5 +8,5 @@ type Resolver struct {
 	MCatalog   map[int]*Catalog       `json:"catalog"`
 	UserCart   map[string][]*CartItem `json:"cartitem,omitempty"`
 	TotalItems map[int]*Item
-	// для мутаций пока резолверы не написанны
+
 }
